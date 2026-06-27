b6ad711ca71d68f1dd0212513b171ecb9a4f86ff
snehasharma272006 <snehasharma272006@gmail.com>
2026-06-27 10:50:44 +0530

    Add files via upload
    problem !

---
+++ ToTestInt.java
@@ -0,0 +1,9 @@
+public class ToTestInt {
+
+    public static void main(String[] args) {
+
+        Arithmetic obj = new Arithmetic();
+
+        System.out.println("Square = " + obj.square(5));
+    }
+}
