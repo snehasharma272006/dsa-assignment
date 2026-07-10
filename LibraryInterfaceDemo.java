f32f8d54e8d032bb3bf9993f2879a362a6ab055c
snehasharma272006 <snehasharma272006@gmail.com>
2026-07-10 10:24:59 +0530

    Update LibraryInterfaceDemo.java

---
+++ LibraryInterfaceDemo.java
@@ -0,0 +1,33 @@
+public class LibraryInterfaceDemo {
+
+    public static void main(String[] args) {
+
+        KidUsers kid = new KidUsers();
+
+        kid.age = 10;
+        kid.registerAccount();
+
+        kid.age = 18;
+        kid.registerAccount();
+
+        kid.bookType = "Kids";
+        kid.requestBook();
+
+        kid.bookType = "Fiction";
+        kid.requestBook();
+
+        AdultUser adult = new AdultUser();
+
+        adult.age = 5;
+        adult.registerAccount();
+
+        adult.age = 23;
+        adult.registerAccount();
+
+        adult.bookType = "Kids";
+        adult.requestBook();
+
+        adult.bookType = "Fiction";
+        adult.requestBook();
+    }
+}
